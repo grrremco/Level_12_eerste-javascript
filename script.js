5adf328d89d654e9c97a015a2cd41796ffc9e577
Remco van de Siepkamp <69791872+grrremco@users.noreply.github.com>
2021-02-06 13:51:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log('Hello Winc Academy')
+
+let name ='Remco';
+console.log(name)
+
+// if kan niet, want dat is een reserved name
+
+console.log(4+4)
+console.log('4'+'4')
+
+console.log(4*4)
+console.log(4-4)
+console.log(4/4)
+console.log(5%2)
+
+//let age = 42;
+//console.log(age)
+//console.log(typeof age)
+
+let age = 'tweeenveertig';
+console.log(age)
+console.log(typeof age)
+
